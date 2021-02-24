4bd7e50afd72bbbcee99dae784ac640db4928426
shubhansu31 <jainshubhansu31@gmail.com>
2021-02-24 21:37:27 +0530

    added API

--- app/src/main/java/com/e/memeapp/MainActivity.kt
+++ app/src/main/java/com/e/memeapp/MainActivity.kt
@@ -2,10 +2,45 @@ package com.e.memeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var memeImageView : ImageView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        memeImageView = findViewById<ImageView>(R.id.memeImageView);
     }
+
+    fun loadMeme(){
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                val url = response.getString(url);
+                Glide.with(this).load(url).into(memeImageView);
+
+            },
+            Response.ErrorListener {
+
+            })
+
+        queue.add(jsonObjectRequest);
+    }
+    fun nextMeme(view: View) {}
+    fun shareMeme(view: View) {}
 }
